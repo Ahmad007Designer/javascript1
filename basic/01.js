315a134fff38abcd9d283e9756ddd6f2649a8fc2
Ahmad007Designer <86216573+Ahmad007Designer@users.noreply.github.com>
2024-02-25 09:42:06 +0000

    container config done

---
+++ basic/01.js
@@ -0,0 +1,16 @@
+//basic syntax--
+console.log("hello Ahmad")
+
+// define variables
+const studentId=51228032
+let studentEmail="ahmadmohdrock@gmail.com" // we always use let variable
+var studentNumber=9335537142
+studentCity="Gorkahpur" // this type of decleration not use wrost code
+// studentId=5122 // const varibles does not change
+
+studentEmail="ahmd@gmail.com"
+studentNumber=9956734241
+/* let and var are but var are fetch problem in scoping
+show we using "let" remove the problem of scoping
+*/
+console.table([studentId,studentEmail,studentNumber,studentCity])
---
+++ basic/datatype.js
@@ -0,0 +1,29 @@
+let name="Ahmad" //string
+let roll=1247 //number
+let isCoder=true //boolean
+let address = null
+let course;
+//null //==> standalone value
+// undefined  //==> when value are not assing
+
+console.log(name)
+console.log(roll)
+console.log(isCoder)
+console.log(address)
+console.log(course)
+
+// ========
+console.log(typeof name)
+console.log(typeof address) //type of null -- object
+console.log(typeof course)
+
+// type conversion ----
+let num=10
+console.log(typeof num)
+num=String(num)
+console.log(typeof num)
+
+let value=null
+let user=undefined
+console.log(typeof value)
+console.log(typeof user)
